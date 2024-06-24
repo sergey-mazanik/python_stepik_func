dc984a33990e404e8e19f3b9e8ede062d02a4f30
Siarhei Mazanik <sergey.mazanik1@gmail.com>
2024-06-24 15:49:32 +0300

    add some tasks for unit 2

---
+++ unit_2.py
@@ -0,0 +1,36 @@
+# def say_hello(name):
+#     print(f"Привет, {name}!")
+#
+#
+# def choose_profession(profession):
+#     print(f'Я хочу стать {profession}ом')
+#
+#
+# say_hello('Артем')
+# choose_profession('Разработчик')
+
+
+# def welcome_to_course(course_name):
+#     print("Приветствую")
+#     print(f'Добро пожаловать на курс "{course_name}"')
+#
+#
+# welcome_to_course('Функции в Python')
+
+
+# def repeat_n_times(n):
+#     for _ in range(n):
+#         print('Я стану программистом')
+#
+#
+# repeat_n_times(5)
+
+
+# def repeat_phrase_n_times(phrase, n):
+#     for _ in range(n):
+#         print(phrase)
+#
+#
+# repeat_phrase_n_times('Белье надевают под одежду', 4)
+
+
