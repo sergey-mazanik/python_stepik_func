64df8720b8cb5de03e6a3d60eb4bf85c238742d3
Siarhei Mazanik <sergey.mazanik1@gmail.com>
2024-06-26 12:22:29 +0300

    add some tasks for unit 3

--- unit_3.py
+++ unit_3.py
@@ -36,3 +36,72 @@
 
 # def is_strings_equal(s1: str, s2: str) -> bool:
 #     return sorted(s1) == sorted(s2)
+
+
+# def words_length(lst: list) -> list:
+#     return [len(i) for i in lst]
+#
+#
+# print(words_length(['Hello', 'world!']))
+# print(words_length(['Python', 'is', 'awesome!']))
+# print(words_length(['You', 'should', 'learn', 'it!']))
+
+
+# def filter_long_words(words: list, num: int) -> list:
+#     return [word for word in words if len(word) > num]
+#
+#
+# print(filter_long_words(['sister', 'arena', 'formal', 'arena', 'spill'], 5))
+# print(filter_long_words(['Python', 'stole', 'my', 'heart'], 4))
+# print(filter_long_words(['ex', 'care', 'room', 'law'], 3))
+
+
+# def is_member(value: str, lst: list) -> bool:
+#     return value in lst
+#
+#
+# print(is_member("e", ['a', 'e', 'i', 'o', 'u']))
+
+
+# def overlapping(lst1: list, lst2: list) -> bool:
+#     flag = False
+#     for i in lst1:
+#         if is_member(i, lst2):
+#             flag = True
+#             break
+#     return flag
+#
+#
+# print(overlapping(['nope', 'nothing', 'in'], ['common']))
+# print(overlapping(['this', 'might', 'work'], ['or', 'maybe', 'this']))
+# print(overlapping(['I', 'think', 'I am', 19], ['19', 'bananas']))
+
+
+# def find_longest_word_len(words: list) -> int:
+#     return len(max(words, key=len))
+#
+#
+# print(find_longest_word_len(['hello', 'world', 'Python', 'reserve']))
+# print(find_longest_word_len(['default', 'ghostwriter', 'bother', 'applaud', 'skate', 'way']))
+# print(find_longest_word_len(['brain', 'candle', 'rare', 'shy']))
+
+
+# def register_check(people_dict: dict):
+#     register_people = {key: value for (key, value) in people_dict.items() if value == 'yes'}
+#     return len(register_people)
+#
+#
+# people = {'Igor': 'yes', 'Stas': 'no', 'Peter': 'no', 'Mary': 'yes'}
+# print(register_check(people))
+# people = {'Igor': 'no', 'Vasya': 'no', 'Peter': 'no', 'Mary': 'no'}
+# print(register_check(people))
+# people = {'Igor': 'yes', 'Vasya': 'yes', 'Peter': 'yes',
+#           'Mary': 'no', 'Alex': 'yes', 'Marina': 'yes'}
+# print(register_check(people))
+
+
+# def create_tuples(lst1: list, lst2: list) -> list:
+#     return list(zip(lst1, lst2))
+#
+#
+# print(create_tuples([1, 2, 3, 4], [5, 6, 7, 8]))
