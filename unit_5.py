ec95a8cc503edfdb86ca9822769a286372aa7661
Siarhei Mazanik <sergey.mazanik1@gmail.com>
2024-07-25 11:06:01 +0300

    add some tasks for unit 5

--- unit_5.py
+++ unit_5.py
@@ -31,3 +31,22 @@
 #             total *= i
 #         cache.setdefault(n, total)
 #     return total
+
+
+# exchange_rates = {
+#     "USD": 1.0,
+#     "EUR": 0.861775,
+#     "GBP": 0.726763,
+#     "INR": 75.054725,
+#     "AUD": 1.333679,
+#     "CAD": 1.237816,
+#     "SGD": 1.346851,
+# }
+#
+#
+# def convert(currency_from: str, currency_to: str, summ: int) -> float:
+#     return round(summ * exchange_rates[currency_to] / exchange_rates[currency_from], 2)
+#
+#
+# currency = convert("GBP", "USD", 100)
+# print(currency)
