d257b135bc482f4daf277d57a1e21885a17ea2e5
Siarhei Mazanik <sergey.mazanik1@gmail.com>
2025-01-28 11:19:02 +0300

    add some tasks for unit 5

--- unit_5.py
+++ unit_5.py
@@ -141,3 +141,19 @@
 # print('-' * 20)
 # lines = [(5, 6), (5, 4), (1, 0), (0, -1), (1, 2), (2, 1)]
 # print(get_sort_lines(lines))
+
+
+# def print_goods(lst: list[dict]):
+#     for item in sorted(lst, key=lambda x: (x['color'].lower(), -x['model'])):
+#         print(f"Производитель: {item.get('make')}, модель: {item.get('model')}, цвет: {item.get('color')}")
+#
+#
+# models = [{'make': 'Nokia', 'model': 2, 'color': 'Black'},
+#           {'make': 'Mi Max', 'model': 3, 'color': 'red'},
+#           {'make': 'Samsung', 'model': 5, 'color': 'Yellow'},
+#           {'make': 'Apple', 'model': 10, 'color': 'RED'},
+#           {'make': 'Oppo', 'model': 9, 'color': 'Red'},
+#           {'make': 'Huawei', 'model': 4, 'color': 'BLACK'},
+#           {'make': 'Honor', 'model': 3, 'color': 'black'},
+#           {'make': 'Nothing Phone', 'model': 7, 'color': 'Yellow'}]
+# print_goods(models)
